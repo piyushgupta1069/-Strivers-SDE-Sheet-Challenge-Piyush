83b6f7879ae33d5ad70f3e1aa09303fce1f5e719
Piyush Gupta <87662348+piyushgupta1069@users.noreply.github.com>
2023-06-18 14:23:33 +0530

    Create Cut logs.cpp

---
+++ Cut logs.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+
+int helper(int e,int f,vector<vector<int>>&dp) {
+    if(f<=1 || e==1) {
+        return dp[e][f]=f;
+    }
+    if(dp[e][f]!=-1) {
+        return dp[e][f];
+    }
+    int ans=1e6,start=1,end=f;
+    while(start<=end){
+        int mid=start+(end-start)/2;
+        int BREAK=helper(e-1,mid-1,dp);
+        int SURVIVE=helper(e,f-mid,dp);
+        ans=min(ans,1+max(BREAK,SURVIVE));
+        if(BREAK<SURVIVE) {
+            start=mid+1;
+        }
+        else {
+            end=mid-1;
+        }
+    }
+    return dp[e][f]=ans;
+}
+
+int cutLogs(int k, int n) {
+    vector<vector<int>>dp(k+1,vector<int>(n+1,-1));
+    return helper(k,n,dp);
+}
